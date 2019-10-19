1f9174f82fdc8ee4ddab92aa48dd39dfa8ac113d
Joshua Sjoding <ga@josh.hey.nu>
2019-10-19 14:25:44 -0700

    smbpacket: Added Data() method to Request and Response

--- smbpacket/request.go
+++ smbpacket/request.go
@@ -5,5 +5,13 @@ type Request []byte
 
 // Header returns the request header of r.
 func (r Request) Header() RequestHeader {
-	return RequestHeader(r[0:64])
+	return RequestHeader(r[0:HeaderSize])
+}
+
+// Data returns the request data that follows the header.
+func (r Request) Data() []byte {
+	if len(r) <= HeaderSize {
+		return nil
+	}
+	return r[HeaderSize:]
 }
--- smbpacket/response.go
+++ smbpacket/response.go
@@ -5,5 +5,13 @@ type Response []byte
 
 // Header returns the response header of r.
 func (r Response) Header() ResponseHeader {
-	return ResponseHeader(r[0:64])
+	return ResponseHeader(r[0:HeaderSize])
+}
+
+// Data returns the response data that follows the header.
+func (r Response) Data() []byte {
+	if len(r) <= HeaderSize {
+		return nil
+	}
+	return r[HeaderSize:]
 }
